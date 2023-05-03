94af5f40775fa697ad8e538dfd490fb5f6221f99
Niyi1974 <103997428+Niyi1974@users.noreply.github.com>
2023-05-03 16:17:37 -0500

    Initial commit

---
+++ src/main/java/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class BankTransactionAnalyzerSimple {
+    private static final String RESOURCES = "src/main/resources";
+
+    public static void main(final String... args) throws IOException{
+
+    }
+}
